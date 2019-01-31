b1499de188abd7918c42141e7597c2267a972b69
Ashly Lau <al5217@ray20.doc.ic.ac.uk>
2019-01-31 17:31:37 +0000

    Implemented self_preservation strategy, cleaned up bloodlust strategy code

--- my_wol.pl
+++ my_wol.pl
@@ -1,4 +1,4 @@
-%:- [war_of_life].
+:- [war_of_life].
 
 %%%%%%%%%%%%%%% test_strategy/3 %%%%%%%%%%%%%%%%%
 test_strategy(N, S1, S2) :-
@@ -33,7 +33,7 @@ count_score(N, S1, S2, NumDraws, NumWinsB, NumWinsR, Longest, Shortest, TotalLen
 	TotalLength is TotalLengthPrev + NumMoves,
 	TotalTime is TotalTimePrev + Runtime.
 
-count_score(0, _, _, 0, 0, 0, 0, 10000, 0, 0). %arbitrarily large max int for initialisation 
+count_score(0, _, _, 0, 0, 0, 0, 250, 0, 0). %arbitrarily large max int for initialisation 
 
 
 
@@ -64,18 +64,16 @@ bloodlust('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
 % chooses Move out of list of Moves that generates board state with
 % fewest number of opponents pieces after turning Conway's crank
 % min_pieces(Colour, CurrentBoard, Moves, Move, MinNumOppPiece)
-min_pieces('b', [AliveBlues, AliveReds], [M|Moves], Move, Min) :-
-	alter_board(M, AliveBlues, NewAliveBlues),
-	next_generation([NewAliveBlues, AliveReds], [_, CrankedAliveReds]),
-	length(CrankedAliveReds, NumOpp),
-	min_pieces('b', [AliveBlues, AliveReds], Moves, PrevMove, PrevMin),
-	(NumOpp < PrevMin -> Move = M, Min is NumOpp; Move = PrevMove, Min is PrevMin).  
-
-min_pieces('r', [AliveBlues, AliveReds], [M|Moves], Move, Min) :-
-	alter_board(M, AliveReds, NewAliveReds),
-	next_generation([AliveBlues, NewAliveReds], [CrankedAliveBlues, _]),
-	length(CrankedAliveBlues, NumOpp),
-	min_pieces('r', [AliveBlues, AliveReds], Moves, PrevMove, PrevMin),
+min_pieces(Colour, [AliveBlues, AliveReds], [M|Moves], Move, Min) :-
+	(Colour == 'b' ->	alter_board(M, AliveBlues, NewAliveBlues),
+										next_generation([NewAliveBlues, AliveReds], [_, CrankedAliveReds]),
+										length(CrankedAliveReds, NumOpp),
+										min_pieces('b', [AliveBlues, AliveReds], Moves, PrevMove, PrevMin)
+										;
+										alter_board(M, AliveReds, NewAliveReds),
+										next_generation([AliveBlues, NewAliveReds], [CrankedAliveBlues, _]),
+										length(CrankedAliveBlues, NumOpp),
+										min_pieces('r', [AliveBlues, AliveReds], Moves, PrevMove, PrevMin)),
 	(NumOpp < PrevMin -> Move = M, Min is NumOpp; Move = PrevMove, Min is PrevMin).  
 
 min_pieces(_, _, [], _, 64).
@@ -85,6 +83,43 @@ min_pieces(_, _, [], _, 64).
 % self preservation
 % chooses next move which (after Conway's crank) produces the board state with the largest number of that player's pieces on the board
 %self_preservation().
+self_preservation('b', [AliveBlues, AliveReds], [NewAliveBlues, AliveReds], Move) :-
+	findall([A,B,MA,MB], (member([A,B], AliveBlues),
+												neighbour_position(A,B,[MA,MB]),
+												\+member([MA,MB], AliveBlues),
+												\+member([MA,MB], AliveReds)),
+			    PossMoves),
+	max_pieces('b', [AliveBlues, AliveReds], PossMoves, Move, _),
+	alter_board(Move, AliveBlues, NewAliveBlues).
+
+self_preservation('r', [AliveBlues, AliveReds], [AliveBlues, NewAliveReds], Move) :-
+	findall([A,B,MA,MB], (member([A,B], AliveReds),
+												neighbour_position(A,B,[MA,MB]),
+												\+member([MA,MB], AliveReds),
+												\+member([MA,MB], AliveBlues)),
+			    PossMoves),
+	max_pieces('r', [AliveBlues, AliveReds], PossMoves, Move, _),
+	alter_board(Move, AliveReds, NewAliveReds).
+
+
+% chooses Move out of list of Moves that generates board state with 
+% most number of player's pieces after turning Conway's Crank
+% max_pieces(Colour, CurrentBoard, Moves, Move, MaxNumPlayerPiece).
+max_pieces(Colour, [AliveBlues, AliveReds], [M|Moves], Move, Max) :-
+	(Colour == 'b' ->	alter_board(M, AliveBlues, NewAliveBlues),
+										next_generation([NewAliveBlues, AliveReds], [CrankedAliveBlues, _]),
+										length(CrankedAliveBlues, NumPlayer),
+										max_pieces('b', [AliveBlues, AliveReds], Moves, PrevMove, PrevMax)
+										;
+										alter_board(M, AliveReds, NewAliveReds),
+										next_generation([AliveBlues, NewAliveReds], [_, CrankedAliveReds]),
+										length(CrankedAliveReds, NumPlayer),
+										max_pieces('r', [AliveBlues, AliveReds], Moves, PrevMove, PrevMax)),
+	(NumPlayer > PrevMax -> Move = M, Max is NumPlayer; Move = PrevMove, Max is PrevMax).
+	
+max_pieces(_, _, [], _, 0).
+
+
 
 
 % land grab
